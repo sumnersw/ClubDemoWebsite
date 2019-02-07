f3496611348c980f0d4c04822bc500a39ecc22bf
Stephen Sumner <jrsumnersw@gmail.com>
2019-02-07 09:33:42 -0500

    uploading files from computer

---
+++ dom.js
@@ -0,0 +1,9 @@
+function openMenuBar(){
+	var dropMenu = document.getElementById('mobile-drop-down');
+	dropMenu.style.display="block";
+}
+
+function closeMenuBar(){
+	var dropMenu = document.getElementById('mobile-drop-down');
+	dropMenu.style.display="none";
+}
